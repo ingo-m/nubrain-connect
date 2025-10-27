c3c8140e00d9abf2d78260d3d71ebf1adc9d24c9
Ingo Marquardt <ingo.marquardt@gmx.de>
2025-10-26 21:07:17 -0400

    WIP: Cached demo mode.

--- app/nubrain/cli.py
+++ app/nubrain/cli.py
@@ -9,6 +9,7 @@ from nubrain.experiment_eeg_to_image_v1.main import experiment_eeg_to_image_v1
 from nubrain.experiment_eeg_to_image_v1.main_autoregressive import (
     experiment_eeg_to_image_v1_autoregressive,
 )
+from nubrain.live_demo.main import run_live_demo
 
 
 def main():
@@ -37,6 +38,12 @@ def main():
         help="Use previous reconstructed image as the next stimulus.",
     )
 
+    parser.add_argument(
+        "--live_demo",
+        type=str,
+        help="Path to the cache for demo.",
+    )
+
     args = parser.parse_args()
 
     print("nubrain")
@@ -49,11 +56,15 @@ def main():
 
     autoregressive = args.autoregressive
 
+    live_demo_cache = args.live_demo
+
     # Load EEG experiment config from yaml file.
     if eeg_to_image_v1:
         # Live EEG to image generation mode. Use corresponding config file loading
         # function (different parameters than regular data collection).
         config = load_config_yaml_eeg_to_image_v1(yaml_file_path=yaml_file_path)
+    elif live_demo_cache:
+        pass
     else:
         # Regular data collection mode.
         config = load_config_yaml(yaml_file_path=yaml_file_path)
@@ -66,6 +77,8 @@ def main():
         else:
             # Live EEG to image generation mode.
             experiment_eeg_to_image_v1(config=config)
+    elif live_demo_cache is not None:
+        run_live_demo(cache=live_demo_cache)
     else:
         # Regular data collection mode.
         experiment(config=config)
