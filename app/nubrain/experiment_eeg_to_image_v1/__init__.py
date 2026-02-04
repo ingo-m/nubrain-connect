139ae3b61d241cdeb492cf798e586d837b87098f
Ingo Marquardt <ingo.marquardt@gmx.de>
2026-02-04 15:58:16 +0100

    Cleaned up imports.

--- app/nubrain/experiment_eeg_to_image_v1/__init__.py
+++ app/nubrain/experiment_eeg_to_image_v1/__init__.py
@@ -1,8 +1,10 @@
-from . import load_config, tone
+# Is this necessary?
+
+# from . import load_config
 
 # Wrap the main import in try, so that the other modules can be imported without
 # dependency on pylsl (e.g. when using global_config during preprocessing).
-try:
-    from . import main
-except Exception as e:
-    print(f"Failed to import nubrain experiment_eeg_to_image_v1 main module: {e}")
+# try:
+#     from . import main
+# except Exception as e:
+#     print(f"Failed to import nubrain experiment_eeg_to_image_v1 main module: {e}")
--- app/nubrain/experiment_image/__init__.py
+++ app/nubrain/experiment_image/__init__.py
@@ -1,8 +1,10 @@
-from . import data, global_config, load_config, randomize_conditions
+# Is this necessary?
+
+# from . import data, global_config, load_config, randomize_conditions
 
 # Wrap the main import in try, so that the other modules can be imported without
 # dependency on pylsl (e.g. when using global_config during preprocessing).
-try:
-    from . import main
-except Exception as e:
-    print(f"Failed to import nubrain main module: {e}")
+# try:
+#     from . import main
+# except Exception as e:
+#     print(f"Failed to import nubrain main module: {e}")
