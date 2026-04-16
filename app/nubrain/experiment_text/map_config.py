c45f0a4e2666e1170316f1c370bfe0e541d39e9d
ingo-m <ingo.marquardt@gmx.de>
2026-04-16 18:02:40 +0200

    Load stimulus data from JSON file, including text and pre-sampled target events (for compatibility with listening condition).

--- app/nubrain/experiment_text/map_config.py
+++ app/nubrain/experiment_text/map_config.py
@@ -7,7 +7,7 @@ def map_session_config_to_experiment_config(
     Map session config to experiment config. Specifically:
     - subject (integer), e.g. 1 -> subject_id (string), e.g. "sub-001"
     - session (integer), e.g. 1 -> session_id (string), e.g. "session-001"
-    - run (integer), e.g. 1 -> word_idx_start (intger) = n_words_to_show * (run - 1)
+    - run (integer), e.g. 1 -> section_idx_start (intger) = n_sections_to_show * (run - 1)
     """
     subject = session_config["subject"]
     session = session_config["session"]
@@ -16,12 +16,12 @@ def map_session_config_to_experiment_config(
     subject_id = f"sub-{subject:03}"
     session_id = f"session-{session:03}"
 
-    n_words_to_show = experiment_config["n_words_to_show"]
-    # Minus one because we start at word zero on first run.
-    word_idx_start = n_words_to_show * (run - 1)
+    n_sections_to_show = experiment_config["n_sections_to_show"]
+    # Minus one because we start at section zero on first run.
+    section_idx_start = n_sections_to_show * (run - 1)
 
     experiment_config["subject_id"] = subject_id
     experiment_config["session_id"] = session_id
-    experiment_config["word_idx_start"] = word_idx_start
+    experiment_config["section_idx_start"] = section_idx_start
 
     return experiment_config
